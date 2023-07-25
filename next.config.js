d95bfb035eaa1bc5c4b8bee7a5bb435b82e935ad
Sharlotte <seeseafood@naver.com>
2023-07-25 20:34:36 +0900

    tried https://github.com/vanilla-extract-css/vanilla-extract/issues/929#issuecomment-1584340099, but failed.

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const withVanillaExtract = createVanillaExtractPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  webpack: (config) => {
+    config.optimization.splitChunks = false;
+
+    return config;
+  },
   reactStrictMode: true,
 };
 
